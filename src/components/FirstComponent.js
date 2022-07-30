e4fef6a84985c997ba71d28fc90bc506d7e5181c
Esteban Herrera <stv.herrera@gmail.com>
2022-07-30 01:03:22 -0500

    🎉 Initial commit and add files

---
+++ src/components/FirstComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const FirstComponent = () => {
+  return (
+    <div>
+      <h1>My First Component</h1>
+        <p> This is the text of my Component</p>
+    </div>
+  )
+}
